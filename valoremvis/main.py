d2211e30e97974d9d6dea917f98f7a2913a57bf1
irdan <daniel1555@gmail.com>
2014-03-26 01:51:46 -0700

    Changing expected data format of get request

--- valoremvis/main.py
+++ valoremvis/main.py
@@ -1,3 +1,4 @@
+import json
 import redis 
 
 from flask import Flask, session, request, abort, jsonify
@@ -11,12 +12,17 @@ app.config.from_object('config')
 @app.route('/<key>')
 def index(key):
     session.permament = True
-    if 'value' not in request.args:
-        value = app.config['CACHE'].get(key) 
+    value = app.config['CACHE'].get(key)
+
+    if 'value[]' in request.args: # adding/updating entry
+        pubkeys = json.loads(value) if value else []
+        values = request.args.getlist('value[]')
+        for v in values:
+            pubkeys.append(v)
+        app.config['CACHE'].set(key, json.dumps(pubkeys))
+        return jsonify({'success': True})
+
+    else: # querying data
         if not value:
             abort(404)
-        return jsonify({'value': value })
-
-    app.config['CACHE'].set(key, request.args.get('value'))
-    return jsonify({'success': True})
-
+        return jsonify({'value': json.loads(value) })
