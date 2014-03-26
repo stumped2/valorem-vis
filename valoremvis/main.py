f25f04459794a4315c0dc43ad203f4af9a95265a
spectralsun <garrett@spectralsun.com>
2014-03-25 22:21:03 -0700

    use redis as value store

--- valoremvis/main.py
+++ valoremvis/main.py
@@ -1,19 +1,22 @@
+import redis 
+
 from flask import Flask, session, request, abort, jsonify
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'This key is really secret NOT'
+app.config.from_object('config')
 
 
 @app.route('/<key>')
 def index(key):
     session.permament = True
     if 'value' not in request.args:
-        if key not in session:
+        value = app.config['CACHE'].get(key) 
+        if not value:
             abort(404)
-        return jsonify({'value': session[key]})
+        return jsonify({'value': value })
 
-    session[key] = request.args.get('value')
+    app.config['CACHE'].set(key, request.args.get('value'))
     return jsonify({'success': True})
 
