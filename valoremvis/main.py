fd921453714ff0da8875b0baf88d93bcb68a45a0
Geoffrey Corey <coreyg@onid.orst.ed>
2014-04-06 16:02:17 -0700

    Got **BASIC** format checking of input of the Directory provider. also restructured the directoy provider to have 2 endpoints: store and search. Can cearch by email (basic email format checking) and search by pgp public key (no format checking for this at the moment).

--- valoremvis/main.py
+++ valoremvis/main.py
@@ -1,48 +1,76 @@
 import json
-import redis 
+import redis
 
 from flask import Flask, session, request, abort, jsonify
 from lepl.apps.rfc3696 import Email
+from base64 import b64decode
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 verifier = Email()
 
-@app.route('/<key>')
-def index(key):
+@app.route('/<action>')
+def index(action):
   session.permament = True
-  values = app.config['CACHE'].get(key)
-  if verify_args(request.args):
-    if len(request.args) == 0:
-      if not values:
+
+  if action == 'store':
+    if verify_store_args(request.args):
+      data = []
+      ia = request.args.getlist('Persona')
+      privly = request.args.getlist('Privly')
+      email_key = get_email_ia(ia[0])
+      pgp_key = get_pgp_ia(privly[0])
+      
+      if email_key is None:
+        abort(400)
+      
+      if pgp_key is None:
+        abort(400)
+
+      data.append(ia[0])
+      data.append(privly[0])
+      app.config['CACHE'].set(email_key, json.dumps(data))
+      app.config['CACHE'].set(pgp_key, json.dumps(data))
+      
+      return jsonify({'success': True})
+  
+    else:
+      abort(400)
+  
+  elif action == 'search':
+    if verify_search_args(request.args):
+      if 'PGP' in request.args:
+        key = request.args.getlist('PGP')
+      elif 'Email' in request.args:
+        key = request.args.getlist('Email')
+      else: #catch all, should never get here because verify_search_args
+        abort(400)
+
+      data = app.config['CACHE'].get(key[0])
+      
+      if data is None:
         abort(404)
-      else:
-        values = json.loads(values)
-        return jsonify({'Persona': values[0], 'Privly': values[1]})
-    ia = request.args.getlist('Persona')
-    privly = request.args.getlist('Privly')
-    ia = ia[0]
-    privly = privly[0]
-    data = []
-    data.append(ia)
-    data.append(privly)
-    app.config['CACHE'].set(key, json.dumps(data))
-    return jsonify({'success': True})
+      
+      data = json.loads(data)
+      return jsonify({'Persona': data[0], 'Privly': data[1]})
+    
+    else:
+      abort(400)
+  
   else:
     abort(400)
 
-
-
 # Get email for key from backed ia
 
-def verify_args(args):
+def verify_store_args(args):
   '''
-  len of ars must be either 2 (storing data) or 0 (queryring data).
+  len of args must be 2.
   "Persona" and "Privly must be keys in args.
+  len of list of data associated with each key must be 1.
   '''
 
-  if len(args) == 2: # storing data
+  if len(args) == 2:
     if 'Persona' in args and 'Privly' in args:
       ia = args.getlist('Persona')
       privly = args.getlist('Privly')
@@ -52,9 +80,70 @@ def verify_args(args):
         return False
     else:
       return False
-
-  elif len(args) == 0: # querying data
-    return True
-
-  else: # catch any unknown things
+  else:
     return False
+
+def verify_search_args(args):
+  '''
+  len of args must be 1.
+  "PGP" or "Email" must be the search by arguement.
+  len of list of data associated with the search key must be 1.
+  If "Email" is the key, verify email sent is a correctly formatted email
+  '''
+
+  verifier = Email()
+  if len(args) == 1:
+    if 'PGP' in args:
+      pgp = args.getlist('PGP')
+      if len(pgp) == 1:
+        return True
+      else:
+        return False
+    elif 'Email' in args:
+      email = args.getlist('Email')
+      if len(email) == 1:
+        if verifier(email[0]):
+          return True
+        else:
+          return False
+      else:
+        return False
+    else:
+      return False
+  else:
+    return False
+
+def get_email_ia(backedia):
+  '''
+  Rudamentory format checking of the backed ia.
+  Given a backed identity assertion, it will extrac the email from it
+  to be used as a key for storage.
+  '''
+
+  verifier = Email()
+  try:
+    ia = backedia.replace('~','.').split('.')
+    cert = b64decode(ia[1])
+    cert = json.loads(cert)
+    if verifier(cert['principal']['email']):
+      return cert['principal']['email']
+    else:
+      return None
+  except:
+    return None
+
+def get_pgp_ia(privlyia):
+  '''
+  Rudamentory format checking for Privly assertion.
+  Will extract a PGP public key to be used as a key in storage.
+  '''
+
+  try:
+    ia = privlyia.split('.')
+    if len(ia) == 2:
+      return ia[0]
+    else:
+      return None
+  except:
+    return None
+# TODO: Verify the pgp pub key and signature match]
